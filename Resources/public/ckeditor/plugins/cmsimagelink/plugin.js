e478ac68fdef6a587806f31aaf04223324975c9a
Tomas Kuba <tomas.kuba@edgedesign.cz>
2013-09-17 16:15:03 +0200

    Fixed some detials and added cms link to image

--- Resources/public/ckeditor/plugins/cmsimagelink/plugin.js
+++ Resources/public/ckeditor/plugins/cmsimagelink/plugin.js
@@ -13,49 +13,44 @@ CKEDITOR.on('dialogDefinition', function(ev){
             if (CKEDITOR.cmsimagelink.images) images = CKEDITOR.cmsimagelink.images;
         }
 
-        infoTab.remove('cmbAlign');
-        infoTab.remove('txtVSpace');
-        infoTab.remove('txtHSpace');
-        infoTab.remove('txtBorder');
-        infoTab.remove('ratioLock');
-        infoTab.remove('txtHeight');
-        infoTab.remove('txtWidth');
-//        infoTab.remove('htmlPreview');
+        infoTab.remove('basic');
         infoTab.get('htmlPreview').style="display: none";
-        console.log(infoTab.get('htmlPreview'));
-        console.log(infoTab);
+        
 
         var values = [];
         for (var key in images) {
             values.push([images[key].title, key]);
         }
 
-        infoTab.add(
-            {
-                type : 'select',
-                id : 'cmsimagelink',
-                label : label,
-                'default' : '',
-                style : 'width:30em',
-                items : values,
-                onChange : function(){
-                    if (this.getValue() in images){
-                        var d = CKEDITOR.dialog.getCurrent();
-                        d.setValueOf('info', 'txtUrl', images[this.getValue()].url);
-                        d.setValueOf('info', 'txtAlt', images[this.getValue()].description);
-                    }
-                },
-                setup : function(data){
-                    this.allowOnChange = false;
-                    this.setValue(data.url ? data.url.url : '');
-                    this.allowOnChange = true;
+        infoTab.add({
+            type : 'select',
+            id : 'cmsimagelink',
+            label : label,
+            'default' : '',
+            style : 'width:30em',
+            items : values,
+            onChange : function(){
+                if (this.getValue() in images){
+                    var d = CKEDITOR.dialog.getCurrent();
+                    d.setValueOf('info', 'txtUrl', images[this.getValue()].url);
+                    d.setValueOf('info', 'txtAlt', images[this.getValue()].description);
+
+                    CKEDITOR.dialog.getCurrent().foreach(function(item){
+                        console.log(item);
+                    });
                 }
+            },
+            setup : function(data){
+                this.allowOnChange = false;
+                this.setValue(data.url ? data.url.url : '');
+                this.allowOnChange = true;
             }
-        );
+        },'htmlPreview');
 
         dialogDefinition.onLoad = function(){
             var internField = this.getContentElement( 'info', 'cmsimagelink' );
             internField.reset();
         };
     }
+
 } );
