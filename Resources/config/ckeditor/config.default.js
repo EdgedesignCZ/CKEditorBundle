d386106ccb5ead59b22d9480323fe26ac01b6bd7
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-03-02 18:35:11 +0100

    updated default config files

---
+++ Resources/config/ckeditor/config.default.js
@@ -0,0 +1,89 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+;(function (CKEDITOR) {
+	'use strict';
+
+	var basePath, extraPlugins, addExternal;
+
+	basePath = CKEDITOR.basePath;
+	basePath = basePath.replace(/ckeditor\/$/,'');
+
+	// Define external plugins for loader
+	addExternal = [
+		{
+			name: 'edgcodesnippet',
+			path: basePath + '../plugins/edgcodesnippet/'
+		},
+		{
+			name: 'cmsfilelink',
+			path: basePath + '../plugins/cmsfilelink/'
+		},
+		{
+			name: 'cmsimagelink',
+			path: basePath + '../plugins/cmsimagelink/'
+		},
+		{
+			name: 'cmslink',
+			path: basePath + '../plugins/cmslink/'
+		},
+		{
+			name: 'showblocks',
+			path: basePath + '../plugins/showblocks/'
+		}
+	];
+
+	// Define extra plugins
+	extraPlugins = [
+		'cmslink',
+		'cmsfilelink',
+		'cmsimagelink',
+		'showblocks',
+		'edgcodesnippet',
+		'image2'
+	];
+
+	// External plugin loader
+	for (var i = 0; i < addExternal.length; i++) {
+		CKEDITOR.plugins.addExternal(addExternal[i].name, addExternal[i].path);
+	};
+
+	CKEDITOR.editorConfig = function( config ) {
+		// Define changes to default configuration here.
+		// For complete reference see:
+		// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+		// Added extra plugins
+		config.extraPlugins = extraPlugins.join(',');
+
+		// The toolbar groups arrangement, optimized for two toolbar rows.
+		config.toolbarGroups = [
+			{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+			{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+			{ name: 'links' },
+			{ name: 'insert' },
+			{ name: 'forms' },
+			{ name: 'tools' },
+			{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+			{ name: 'others' },
+			'/',
+			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+			{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+			{ name: 'styles' },
+			{ name: 'colors' },
+			{ name: 'about' }
+		];
+
+		// Remove some buttons provided by the standard plugins, which are
+		// not needed in the Standard(s) toolbar.
+		config.removeButtons = 'Underline,Subscript,Superscript';
+
+		// Set the most common block elements.
+		config.format_tags = 'p;h1;h2;h3;pre';
+
+		// Simplify the dialog windows.
+		config.removeDialogTabs = 'image:advanced;link:advanced';
+	};
+}(CKEDITOR));
