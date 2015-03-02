201f349cbe9fa2b63e6cf42b2ca8ab1e00a9e27d
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-03-02 18:36:19 +0100

    Created plugins folder for external ckeditor plugins

---
+++ Resources/plugins/edgcodesnippet/lang/cs.js
@@ -0,0 +1,5 @@
+﻿/*
+ Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang("edgcodesnippet","cs",{button:"Vložit úryvek kódu",codeContents:"Obsah kódu",emptySnippetError:"Úryvek kódu nemůže být prázdný.",emptyLangError:"Vyberte jazyk v jakém je úryvek kódu napsaný.",language:"Jazyk",title:"Úryvek kódu",pathName:"úryvek kódu"});
