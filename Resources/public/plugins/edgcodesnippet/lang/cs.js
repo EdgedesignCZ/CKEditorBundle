fc72e90ea15602a472186e46eb9f6e4739b91a8f
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-04-03 14:46:32 +0200

    fixnuty prekladove soubory pro cs a en

--- Resources/public/plugins/edgcodesnippet/lang/cs.js
+++ Resources/public/plugins/edgcodesnippet/lang/cs.js
@@ -2,4 +2,12 @@
  Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("edgcodesnippet","cs",{button:"Vložit úryvek kódu",codeContents:"Obsah kódu",emptySnippetError:"Úryvek kódu nemůže být prázdný.",emptyLangError:"Vyberte jazyk v jakém je úryvek kódu napsaný.",language:"Jazyk",title:"Úryvek kódu",pathName:"úryvek kódu"});
+CKEDITOR.plugins.setLang("edgcodesnippet","cs",{
+    button:"Vložit úryvek kódu",
+    codeContents:"Obsah kódu",
+    emptySnippetError:"Úryvek kódu nemůže být prázdný.",
+    emptyLangError:"Vyberte jazyk v jakém je úryvek kódu napsaný.",
+    language:"Jazyk",
+    title:"Úryvek kódu",
+    pathName:"úryvek kódu"
+});
