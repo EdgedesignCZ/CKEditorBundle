accf9e7b06bff988386bc3eb38d3f6f4cbe5450b
Daniel Sitek <dan.sitek@edgedesign.cz>
2015-04-20 09:39:15 +0200

    smazany vsechny jazykove verze krom cs a en

--- Resources/public/plugins/edgcodesnippet/lang/cs.js
+++ Resources/public/plugins/edgcodesnippet/lang/cs.js
@@ -2,7 +2,7 @@
  Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("edgcodesnippet","cs",{
+CKEDITOR.plugins.setLang("edgcodesnippet", "cs", {
     button:"Vložit úryvek kódu",
     codeContents:"Obsah kódu",
     emptySnippetError:"Úryvek kódu nemůže být prázdný.",
